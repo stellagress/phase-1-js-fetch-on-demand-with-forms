f4dc658d5c52adbdf7bb0129853a11eb16492e7d
Stella Mazzillo <stellagress5@gmail.com>
2023-04-18 15:43:22 -0400

    complete fetch on demand with forms

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
 const init = () => {
+
+    const inputForm = document.querySelector('form')
+
+    inputForm.addEventListener('submit', event => 
+    {event.preventDefault()
+
+    // const input = event.target.children[1].value 
+    // console.log(input)
+    // another alternative for the following line:
+
+    const input = document.querySelector('input#searchByID')        
+        console.log(input.value)
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(res => res.json())
+    .then(data => {
+        const title = document.querySelector('section#movieDetails h4')
+        const summary = document.querySelector('section#movieDetails p')
+
+        title.innerText = data.title
+        summary.innerText = data.summary
+
+
+    })
+        
+    })
+
+
   
 }
 
